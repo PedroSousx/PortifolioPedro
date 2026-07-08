382594bd2794323baf58f6f87d5814a1b4ddd1d8
Pedro <pedroh.scintra@gmail.com>
2026-07-08 19:15:55 -0300

    feat: deploy para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/PortifolioPedro',
+//   build: {
+//     outDir: 'docs'
+//   }
+// })  
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PortifolioPedro',
+  base: '/',
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   }
-})  
+})
